9133c029fbd4f6abaf59ca4bb675d38d1bc03cdb
matouskunvalsky <kunvma@delta-studenti.cz>
2026-09-24 08:18:05 +0200

    Change currency check in add method
    Updated currency check in add method from 'CZ' to 'usd'.

--- Calculator.java
+++ Calculator.java
@@ -19,7 +19,7 @@ public class Calculator {
 
     public int add (int d, int c) {
 
-        if (this.currency.equals("CZ")) {
+        if (this.currency.equals("usd")) {
             // ....
         }
 
